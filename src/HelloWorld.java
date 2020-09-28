77effd3ee4c6b3cf2f13dace5a4f5fb3f78c03a5
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-28 15:53:52 -0500

    finished Data Type Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,46 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        System.out.print("Hello, ");
+//        System.out.print("World!");
+
+        int myFavoriteNumber = 23;
+//        System.out.print(myFavoriteNumber);
+
+        String myString = "bianca";
+//        System.out.print(myString);
+
+        double myNumber = 3.14;
+//        System.out.print(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4 + 5;
+//        System.out.print(x);
+
+//        int x = 3;
+//        int y = 4 * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+        byte test = 128;
+//        ERROR = java: incompatible types: possible lossy conversion from int to byte, highest = 127
+        short testing = 32767;
+        int anotherOne = 2147483647;
+        long testers = 999999999;
+        System.out.print(test);
     }
 }
