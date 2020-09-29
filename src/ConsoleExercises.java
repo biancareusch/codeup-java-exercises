42459c2f459911135a49e935fe05f6e3d88cb9fb
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-29 14:54:24 -0500

    feat: program now has instructions

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,31 +5,36 @@ import java.util.SortedMap;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %.2f.", pi);
+        System.out.format("The value of pi is approximately %.2f.\n", pi);
 
         Scanner sc = new Scanner(System.in);
 
-//        System.out.print("\nType in any number.\n");
-//        int userInput = sc.nextInt();
-//        System.out.format("\n You picked %d.",userInput);
+        System.out.print("\nType in any number:");
+        int userInput = sc.nextInt();
+        System.out.format("Your number is: %d.\n",userInput);
 
-//        System.out.print("Type in a word.");
-//        String input1 = sc.next();
-//        System.out.print("Type in a word.");
-//        String input2 = sc.next();
-//        System.out.print("Type in a word.");
-//        String input3 = sc.next();
-//        System.out.format("Here are your three words: %s %s %s.",input1, input2, input3);
+        System.out.print("\nThis program will take in three words and return them to you.");
+        System.out.print("\nType in first word:");
+        String input1 = sc.next();
+        System.out.print("Type in second word:");
+        String input2 = sc.next();
+        System.out.print("Type in third word:");
+        String input3 = sc.next();
+        System.out.format("Here are your three words: %s %s %s.\n",input1, input2, input3);
 
-//        System.out.print("\nType in a sentence.\n");
-//        String userString = sc.nextLine();
-//        System.out.format("Here's your sentence: %s.",userString);
+        sc.nextLine();
+        System.out.print("\nThis program takes in a sentence and returns it to you.");
+        System.out.print("\nType in a sentence.\n");
+        String userString = sc.nextLine();
+        System.out.format("Here's your sentence: %s.\n",userString);
 
-        System.out.print("Length\n");
+
+        System.out.print("\nThis program wil take in length, width and height, to calculate perimeter, area and volume\n");
+        System.out.print("Length:\n");
         String lengthInput = sc.nextLine();
-        System.out.print("Width\n");
+        System.out.print("Width:\n");
         String widthInput = sc.nextLine();
-        System.out.print("Height\n");
+        System.out.print("Height:\n");
         String heightInput = sc.nextLine();
         float length = Float.parseFloat(lengthInput);
         float width = Float.parseFloat(widthInput);
@@ -39,6 +44,6 @@ public class ConsoleExercises {
         float volume = length * width * height;
         System.out.format("Perimeter: %.2f. Area: %.2f. Volume: %.2f", perimeter, area, volume);
 
-//        sc.useDelimiter("\n");
+        sc.useDelimiter("\n");
     }
 }
