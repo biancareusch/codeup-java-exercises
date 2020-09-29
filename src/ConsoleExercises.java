3b0d4a506e0ad2a6f595ee4c44dc298c5cc88d7c
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-29 13:58:36 -0500

    feat: takes in three seperate words and prints them together in one statement

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f.", pi);
+//        System.out.format("The value of pi is approximately %.2f.", pi);
 
         Scanner sc = new Scanner(System.in);
 
@@ -10,9 +10,18 @@ public class ConsoleExercises {
 //        int userInput = sc.nextInt();
 //        System.out.format("\n You picked %d.",userInput);
 
-        System.out.print("\nType in a sentence.\n");
-        String userString = sc.nextLine();
-        System.out.format("Here's your sentence: %s.",userString);
+        System.out.print("Type in a word.");
+        String input1 = sc.next();
+        System.out.print("Type in a word.");
+        String input2 = sc.next();
+        System.out.print("Type in a word.");
+        String input3 = sc.next();
+        System.out.format("Here are your three words:%s %s %s.",input1, input2, input3);
+
+//        System.out.print("\nType in a sentence.\n");
+//        String userString = sc.nextLine();
+//        System.out.format("Here's your sentence: %s.",userString);
+
 
     }
 }
