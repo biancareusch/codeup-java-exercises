1acfda755479251151f5f8dc1b54e546d9c7636a
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-29 14:12:58 -0500

    feat: now takes in length and width to calculate print out out area and perimeter

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,4 +1,7 @@
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
+import java.util.SortedMap;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
@@ -10,17 +13,25 @@ public class ConsoleExercises {
 //        int userInput = sc.nextInt();
 //        System.out.format("\n You picked %d.",userInput);
 
-        System.out.print("Type in a word.");
-        String input1 = sc.next();
-        System.out.print("Type in a word.");
-        String input2 = sc.next();
-        System.out.print("Type in a word.");
-        String input3 = sc.next();
-        System.out.format("Here are your three words:%s %s %s.",input1, input2, input3);
+//        System.out.print("Type in a word.");
+//        String input1 = sc.next();
+//        System.out.print("Type in a word.");
+//        String input2 = sc.next();
+//        System.out.print("Type in a word.");
+//        String input3 = sc.next();
+//        System.out.format("Here are your three words: %s %s %s.",input1, input2, input3);
 
 //        System.out.print("\nType in a sentence.\n");
 //        String userString = sc.nextLine();
 //        System.out.format("Here's your sentence: %s.",userString);
+
+        System.out.print("Length\n");
+        int length = sc.nextInt();
+        System.out.print("Width\n");
+        int width = sc.nextInt();
+        int area = length * width;
+        int perimeter = (2 * length) + (2 * width);
+        System.out.format("Perimeter: %d. Area: %d.", perimeter, area);
 
 
     }
