daeed2631dcc0686581ffc14ded7578d1f0f34d0
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-01 14:20:39 -0500

    finished last exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,29 +1,54 @@
 import java.util.Scanner;
 
 public class ControlFlowExercises {
-    //Prompt the user to enter an integer.
-//Display a table of squares and cubes from 1 to the value entered.
-//Ask if the user wants to continue.
-//Assume that the user will enter valid data.
-//Only continue if the user agrees to.
+//    Prompt the user for a numerical grade from 0 to 100.
+//    Display the corresponding letter grade.
+//    Prompt the user to continue.
+//    Assume that the user will enter valid integers for the grades.
+//    The application should only continue if the user agrees to.
+//    Grade Ranges:
+//
+//    A : 100 - 88
+//    B : 87 - 80
+//    C : 79 - 67
+//    D : 66 - 60
+//    F : 59 - 0
     public static void main(String[] args) {
         boolean confirmation;
 
         do {
             Scanner sc = new Scanner(System.in);
-            System.out.println("What number would you like to go up to? ");
-            long userInput = sc.nextInt();
+            System.out.println("Type in your numerical grade: ");
+            int userInput = sc.nextInt();
 
-            System.out.println("Here is your table! \n");
-            System.out.println("\nnumber | squared | cubed");
-            System.out.println("-------|---------|------");
-
-            for (long num = 1; num <= userInput; num++) {
-                long resultSquared = num * num;
-                long resultCubed = num * num * num;
-
-                System.out.println(num + "      | " + resultSquared + "       | " + resultCubed);
+            if(userInput > 99){
+                System.out.println("You have an A+!");
+            } else if(userInput >= 90 && userInput < 99){
+                System.out.println("You have an A!");
+            }else if(userInput >= 88 && userInput < 90){
+                System.out.println("You have an A-!");
+            }else if(userInput >= 86 && userInput < 88){
+                System.out.println("You have an B+!");
+            }else if(userInput >= 82 && userInput < 86){
+                System.out.println("You have an B!");
+            }else if(userInput >= 80 && userInput < 82){
+                System.out.println("You have an B-!");
+            }else if(userInput >= 78 && userInput <80){
+                System.out.println("You have an C+!");
+            }else if(userInput >= 69 && userInput < 78){
+                System.out.println("You have an C!");
+            }else if(userInput >= 67 && userInput < 69){
+                System.out.println("You have an C-!");
+            }else if(userInput >= 65 && userInput < 67){
+                System.out.println("You have an D+!");
+            }else if(userInput >= 62 && userInput < 65){
+                System.out.println("You have an D!");
+            }else if(userInput >= 60 && userInput < 62){
+                System.out.println("You have an D-!");
+            }else if(userInput < 60){
+                System.out.println("You have an F!");
             }
+
 
             System.out.println("Do you want to continue? [y/N]");
             String keepGoing = sc.next();
