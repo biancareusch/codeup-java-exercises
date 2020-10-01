da0c30094f6743b833caf2d3690b368417b0e606
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-01 13:54:23 -0500

    finished display table of powers exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,32 +1,34 @@
 import java.util.Scanner;
 
 public class ControlFlowExercises {
-    public static void main(String[] args) {
-//Prompt the user to enter an integer.
+    //Prompt the user to enter an integer.
 //Display a table of squares and cubes from 1 to the value entered.
 //Ask if the user wants to continue.
 //Assume that the user will enter valid data.
 //Only continue if the user agrees to.
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        boolean confirmation;
 
-        System.out.println("What number would you like to go up to? ");
-        long userInput = sc.nextInt();
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("What number would you like to go up to? ");
+            long userInput = sc.nextInt();
 
-        System.out.println("Here is your table! \n");
-        System.out.println("\nnumber | squared | cubed");
-        System.out.println("-------|---------|------");
+            System.out.println("Here is your table! \n");
+            System.out.println("\nnumber | squared | cubed");
+            System.out.println("-------|---------|------");
 
-        for (long num = 1; num <= userInput; num++) {
-            long resultSquared = num * num;
-            long resultCubed = num * num * num;
+            for (long num = 1; num <= userInput; num++) {
+                long resultSquared = num * num;
+                long resultCubed = num * num * num;
 
-            System.out.println(num + "      | " + resultSquared + "       | " + resultCubed);
-        }
-        System.out.println("Do you want to continue? [y/N]");
-        String keepGoing = sc.next();
-        boolean confirmation = keepGoing.equalsIgnoreCase("y");
-//        if (confirmation) {
-//
-//        }
+                System.out.println(num + "      | " + resultSquared + "       | " + resultCubed);
+            }
+
+            System.out.println("Do you want to continue? [y/N]");
+            String keepGoing = sc.next();
+            confirmation = keepGoing.equalsIgnoreCase("y");
+        } while (confirmation);
+
     }
 }
