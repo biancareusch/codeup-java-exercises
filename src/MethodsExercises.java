e2ecdaad09173524b8ec3fd357557b01827427b6
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-02 11:12:21 -0500

    working on Basic Arithmetic Bonus

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,10 +1,11 @@
 public class MethodsExercises {
     public static void main(String[] args) {
-adding(2,6);
-subtracting(9,2);
-multiplication(6,4);
-division(9,3);
-modulus(9,4);
+        adding(2, 6);
+        subtracting(9, 2);
+        multiplication(6, 4);
+        division(9, 3);
+        modulus(9, 4);
+        multi(3, 5);
     }
 
     public static void adding(int a, int b) {
@@ -34,30 +35,23 @@ modulus(9,4);
 
 
 
+    public static void multi(int factor, int base) {
+        if (factor == 0) {
+            System.out.println(0);
+        } else if (factor == 1) {
+            System.out.println(base);
+        } else if (factor == 2) {
+            System.out.println(base + base);
+        } else if (factor == 3){
+            System.out.println(base + base + base);
+        } else if (factor == 4){
+            System.out.println(base + base + base + base);
+        }else{
+            System.out.println("sorry...");
+        }
 
-
-
-
-
-//    public static void sayHello(int times) {
-//        for (int i = 0; i < times; i += 1) {
-//            sayHello();
-//        }
-//    }
-//    // version 2
-//    public static void sayHello() {
-//        sayHello("Hello", "World");
-//    }
-//    // version 3
-//    public static void sayHello(String name) {
-//        sayHello("Hello", name);
-//    }
-//    // version 4
-//    public static void sayHello(String greeting, String name) {
-//        System.out.println(greeting + ", " + name + "!");
-//    }
+    }
 }
-
 
 
 
