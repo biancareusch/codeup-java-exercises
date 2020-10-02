fcb6f1f88c84e4e857443377147ca2bd30f597d5
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-02 10:37:27 -0500

    finished Basic Arithmetic exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,63 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+adding(2,6);
+subtracting(9,2);
+multiplication(6,4);
+division(9,3);
+modulus(9,4);
+    }
+
+    public static void adding(int a, int b) {
+        int result = a + b;
+        System.out.println(result);
+    }
+
+    public static void subtracting(int a, int b) {
+        int result = a - b;
+        System.out.println(result);
+    }
+
+    public static void multiplication(int a, int b) {
+        int result = a * b;
+        System.out.println(result);
+    }
+
+    public static void division(int a, int b) {
+        int result = a / b;
+        System.out.println(result);
+    }
+
+    public static void modulus(int a, int b) {
+        int result = a % b;
+        System.out.println(result);
+    }
+
+
+
+
+
+
+
+
+//    public static void sayHello(int times) {
+//        for (int i = 0; i < times; i += 1) {
+//            sayHello();
+//        }
+//    }
+//    // version 2
+//    public static void sayHello() {
+//        sayHello("Hello", "World");
+//    }
+//    // version 3
+//    public static void sayHello(String name) {
+//        sayHello("Hello", name);
+//    }
+//    // version 4
+//    public static void sayHello(String greeting, String name) {
+//        System.out.println(greeting + ", " + name + "!");
+//    }
+}
+
+
+
+
