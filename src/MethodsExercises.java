0651fadfb6f09d3ef279f195ac0cecf6a810d784
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-05 09:57:40 -0500

    getInteger now uses recursion

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,7 +4,9 @@ import java.lang.Math;
 public class MethodsExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("multi  = " + multi(3,4));
 
+        getInteger(2,10);
         //getInteger
 //        System.out.print("Enter a number between 1 and 10: ");
 //        int userInput = getInteger(1, 10);
@@ -20,16 +22,16 @@ public class MethodsExercises {
 //        } while (confirmation);
 
         // Dice
-        do{
-        System.out.print("Enter the number of sides for a pair of dice: ");
-        int n = sc.nextInt();
-        int dice1 = dice(n);
-        int dice2 = dice(n);
-            System.out.println(dice1 + " + " + dice2 + " = " + (dice1 + dice2));
-            System.out.println("\nDo you want to try again? [y/N]");
-            String keepGoing = sc.next();
-            confirmation = keepGoing.equalsIgnoreCase("y");
-        }while(confirmation);
+//        do{
+//        System.out.print("Enter the number of sides for a pair of dice: ");
+//        int n = sc.nextInt();
+//        int dice1 = dice(n);
+//        int dice2 = dice(n);
+//            System.out.println(dice1 + " + " + dice2 + " = " + (dice1 + dice2));
+//            System.out.println("\nDo you want to try again? [y/N]");
+//            String keepGoing = sc.next();
+//            confirmation = keepGoing.equalsIgnoreCase("y");
+//        }while(confirmation);
     }
 
     public static int dice( int n) {
@@ -46,12 +48,16 @@ public class MethodsExercises {
 
     public static int getInteger(long min, long max) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number between " + min + " and "+ max);
         long userInput = scan.nextInt();
-        if (userInput > min && userInput < max) {
-            System.out.println("Valid number.");
-        } else {
-            System.out.println("Invalid number.\n Try again: ");
+        if (userInput < min) {
+            System.out.println("Number is less than " + min);
+            getInteger(min,max);
+        } else if(userInput > max){
+            System.out.println("Number is more than " + max);
             getInteger(min, max);
+        } else {
+            System.out.println("Thank you");
         }
         return (int) userInput;
     }
@@ -86,20 +92,29 @@ public class MethodsExercises {
         return result;
     }
 
-    public static void multi(int factor, int base) {
-        if (factor == 0) {
-            System.out.println(0);
-        } else if (factor == 1) {
-            System.out.println(base);
-        } else if (factor == 2) {
-            System.out.println(base + base);
-        } else if (factor == 3) {
-            System.out.println(base + base + base);
-        } else if (factor == 4) {
-            System.out.println(base + base + base + base);
-        } else {
-            System.out.println("sorry...");
+    public static int multi(int factor, int base) {
+        int result = 0;
+        for(int i = 1; i < base;i++){
+            result = factor++;
         }
+        return result;
+
+
+
+//        if (factor == 0) {
+//            System.out.println(0);
+//            return 0;
+//        } else if (factor == 1) {
+//            System.out.println(base);
+//        } else if (factor == 2) {
+//            System.out.println(base + base);
+//        } else if (factor == 3) {
+//            System.out.println(base + base + base);
+//        } else if (factor == 4) {
+//            System.out.println(base + base + base + base);
+//        } else {
+//            System.out.println("sorry...");
+//        }
     }
 }
 
