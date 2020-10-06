7c8a4a1e71da4acee0aca2de55bd1e5a4549e660
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-06 10:49:37 -0500

    added optional parameter for prompts to every method

--- src/util/Input.java
+++ src/util/Input.java
@@ -13,6 +13,13 @@ public class Input {
         return userInput;
     }
 
+    public String getString(String prompt) {
+        System.out.println("prompt = " + prompt);
+        System.out.println("Enter a sentence :");
+        String userInput = scanner.nextLine();
+        System.out.println("Your sentence is ' " + userInput + "'.");
+        return userInput;
+    }
     public boolean yesNo() {
         System.out.println("Yes or No?");
         String userInput = scanner.nextLine();
@@ -25,8 +32,35 @@ public class Input {
         }
     }
 
+    public boolean yesNo(String prompt) {
+        System.out.println("prompt = " + prompt);
+        System.out.println("Yes or No?");
+        String userInput = scanner.nextLine();
+        if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
+            System.out.println(true);
+            return true;
+        } else {
+            System.out.println(false);
+            return false;
+        }
+    }
     public int getInt(int min, int max) {
 
+        System.out.println("Enter a number between " + min + " and " + max + " :");
+        int input = scanner.nextInt();
+        if (input < min) {
+            System.out.println("Your number needs to be higher.");
+            getInt(min, max);
+        } else if (input > max) {
+            System.out.println("Your number needs to be lower.");
+            getInt(min, max);
+        } else {
+            System.out.println("Your number is " + input);
+        }
+        return input;
+    }
+    public int getInt(int min, int max, String prompt) {
+        System.out.println("prompt = " + prompt);
         System.out.println("Enter a number between " + min + " and " + max + " :");
         int input = scanner.nextInt();
         if (input < min) {
@@ -47,10 +81,31 @@ public class Input {
         System.out.println("Your number is " + input);
         return input;
     }
-
+    public int getInt(String prompt) {
+        System.out.println("prompt = " + prompt);
+        System.out.println("Enter a number : ");
+        int input = scanner.nextInt();
+        System.out.println("Your number is " + input);
+        return input;
+    }
 
     public double getDouble(double min, double max) {
 
+        System.out.println("Enter a double between " + min + " and " + max + " :");
+        double input = scanner.nextDouble();
+        if (input < min) {
+            System.out.println("Your number needs to be higher.");
+            getDouble(min, max);
+        } else if (input > max) {
+            System.out.println("Your number needs to be lower.");
+            getDouble(min, max);
+        } else {
+            System.out.println("Your number is " + input);
+        }
+        return input;
+    }
+    public double getDouble(double min, double max, String prompt) {
+        System.out.println("prompt = " + prompt);
         System.out.println("Enter a double between " + min + " and " + max + " :");
         double input = scanner.nextDouble();
         if (input < min) {
@@ -72,4 +127,11 @@ public class Input {
         return input;
     }
 
+    public double getDouble(String prompt) {
+        System.out.println("prompt = " + prompt);
+        System.out.println("Enter a double :");
+        double input = scanner.nextDouble();
+        System.out.println("Your number is " + input);
+        return input;
+    }
 }
