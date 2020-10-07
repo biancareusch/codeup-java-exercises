0ef839246434c828a9ab11dcad71549f4f748fd8
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-07 11:37:48 -0500

    still working on addPerson method

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,31 +1,37 @@
 import java.util.Arrays;
 
-public class ArraysExercises extends Person{
+public class ArraysExercises extends Person {
     public static void main(String[] args) {
 
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(numbers));
 
-        String[] Persons = {"john", "bob", "mike"};
-        Person person2 = new Person(Persons[0]);
-        Person person3 = new Person(Persons[1]);
-        Person person4 = new Person(Persons[2]);
 
-        for(String person : Persons){
-            System.out.println("person = " + person);
+        Person[] persons = new Person[3];
+
+        persons[0] = new Person("Fer");
+        persons[1] = new Person("John");
+        persons[2] = new Person("Bianca");
+
+        for (Person person : persons) {
+            person.sayHello();
         }
+
+int n = 5;
+        System.out.println(Arrays.toString(addPerson(n, persons, new Person("Lisa"))));
 
     }
 
 
-//    public static Arrays addPerson(Arrays Persons, Person name){
-//        String personName = name.toString();
-//        Person newPerson = new Person(personName);
-//
-//        String[] newPersons ;
-//
-//        Arrays.fill(newPersons, Persons.name);
-//
-//        return Arrays newPersons;
-//    }
+    public static Person[] addPerson(int n, Person[] persons, Person name) {
+
+        Person newArray[] = new Person[n + 1];
+        for (int i = 0; i < n; i++) {
+            newArray[i] = persons[i];
+
+            newArray[n] = name;
+        }
+        return newArray;
+
+    }
 }
