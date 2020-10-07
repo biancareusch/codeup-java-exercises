909b2ff45eac19c6b333240654d8042fdc19803f
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-07 12:13:08 -0500

    starting movieApplication exercise

---
+++ src/movies/Movie.java
@@ -0,0 +1,28 @@
+package movies;
+
+public class Movie {
+    private String name;
+    private String category;
+
+//    public Movies(String name, String category) {
+//        this.name = name;
+//        this.category = category;
+//    }
+
+    public String getMovieName(){
+        return this.name ;
+    }
+
+    public String getMovieCategory(){
+        return this.category;
+    }
+
+    public void setMovieName(String name){
+        this.name = name;
+    }
+
+    public void setMovieCategory(String category){
+        this.category = category;
+    }
+
+}
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,28 @@
+package movies;
+import util.Input;
+
+import java.util.Scanner;
+
+public class MoviesApplication {
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+        System.out.println(
+                "\nWhat would you like to do?" +
+                "\n0 - exit" +
+                "\n1 - view all movies" +
+                "\n2 - view movies in animated category" +
+                "\n3 - view movies in drama category" +
+                "\n4 - view movies in horror category" +
+                "\n5 - view movies in sci-fi category" +
+                "\n\nEnter your choice"
+        );
+//        String input = sc.next();
+
+
+    }
+
+
+
+
+}
